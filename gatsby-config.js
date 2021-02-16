f648084a193474dd107414d9dba99423d1baeb6d
Yvonne Tang <ytang2@stanford.edu>
2021-02-15 17:40:00 -0800

    remove boilerplate api keys

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,64 @@
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: `Filter Storyblok`,
+    description: `Testing`,
   },
   plugins: [
+    `gatsby-plugin-anchor-links`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [
+          { userAgent: '*', allow: '/' },
+          { userAgent: '*', disallow: '/editor/' }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [
+          `/editor/*`,
+          `/editor/`,
+          `/editor`,
+          `/global-components/*`,
+          `/global-components`,
+        ],
+      }
+    },
     {
       resolve: 'gatsby-source-storyblok',
       options: {
-        accessToken: 'bJQb8KcUXW4NJ35XJFwGuwtt',
+        accessToken: process.env.GATSBY_STORYBLOK_ACCESS_TOKEN,
         homeSlug: 'home',
-        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft'
+        resolveRelations: [],
+        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft'  // show only published on the front end site
+        // version: 'draft'  // would show any including drafts
       }
     },
-    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `Make a Gift`,
+        start_url: `/`,
+        include_favicon: false,
+        crossOrigin: `use-credentials`,
+        icons: [],
       },
     },
-    {
-      resolve: `gatsby-transformer-sharp`
-    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
